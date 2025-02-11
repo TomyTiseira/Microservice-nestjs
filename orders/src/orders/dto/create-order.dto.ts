7fd3a1225a4c1c5cb75a30fe94bf5ea3d7395164
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-02-11 05:57:58 -0300

    Add Prisma integration and create Order model with validation in CreateOrderDto

--- orders/src/orders/dto/create-order.dto.ts
+++ orders/src/orders/dto/create-order.dto.ts
@@ -1,1 +1,22 @@
-export class CreateOrderDto {}
+import { OrderStatus } from "@prisma/client";
+import { IsBoolean, IsEnum, IsNumber, IsOptional, IsPositive } from "class-validator";
+
+export class CreateOrderDto {
+  @IsNumber()
+  @IsPositive()
+  totalAmount: number;
+
+  @IsNumber()
+  @IsPositive()
+  totalItems: number;
+
+  @IsEnum(OrderStatus, {
+    message: `Status must be one of the following values: ${Object.values(OrderStatus).join(', ')}`
+  })
+  @IsOptional()
+  status: OrderStatus = OrderStatus.PENDING;
+
+  @IsBoolean()
+  @IsOptional()
+  paid: boolean = false;
+}
