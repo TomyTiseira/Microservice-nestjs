ce1c4ace90c9783674daf82cef82697c21105cc8
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-02-11 05:57:37 -0300

    Refactor product DTOs and update products controller to use consolidated imports

--- client-gateway/src/products/products.controller.ts
+++ client-gateway/src/products/products.controller.ts
@@ -3,7 +3,7 @@ import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError } from 'rxjs';
 import { PaginationDto } from 'src/common';
 import { PRODUCT_SERVICE } from 'src/config';
-import { CreateProductDto } from './dto/create-product.dto';
+import { CreateProductDto, UpdateProductDto } from './dto';
 
 @Controller('products')
 export class ProductsController {
@@ -38,7 +38,7 @@ export class ProductsController {
   @Patch(':id')
   updateProduct(
     @Param('id', ParseIntPipe) id: number,
-    @Body() updateProductDto: any) {
+    @Body() updateProductDto: UpdateProductDto,) {
     return this.productService.send(
       { cmd: 'update_product' },
       { id, ...updateProductDto },
