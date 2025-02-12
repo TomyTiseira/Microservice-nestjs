24ac971f874380de38f50d79802cef7c1ac4a237
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-02-12 05:16:16 -0300

    Add OrderStatusDto and update orders controller to handle order status queries and updates

---
+++ client-gateway/src/orders/dto/order-status.dto.ts
@@ -0,0 +1,8 @@
+import { IsEnum, IsOptional } from "class-validator";
+import { OrderStatus } from "../enum/order.enum";
+
+export class OrderStatusDto {
+  @IsOptional()
+  @IsEnum(OrderStatus, { message: `Status must be one of the following values: ${Object.values(OrderStatus).join(', ')}`})
+  status: OrderStatus;
+}
--- client-gateway/src/orders/orders.controller.ts
+++ client-gateway/src/orders/orders.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Body, Param, Inject } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query, Patch } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { ORDER_SERVICE } from 'src/config';
 import { catchError } from 'rxjs';
-import { CreateOrderDto } from './dto';
+import { CreateOrderDto, OrderStatusDto } from './dto';
+import { PaginationDto } from 'src/common';
 
 @Controller('orders')
 export class OrdersController {
@@ -16,15 +17,35 @@ export class OrdersController {
   }
 
   @Get()
-  findAll() {
-    return this.ordersService.send('findAllOrders', {});
+  findAll(@Query() paginationDto: PaginationDto) {
+    return this.ordersService.send('findAllOrders', paginationDto);
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.send('findOneOrder', id)
-      .pipe(
-        catchError(error => { throw new RpcException(error) })
-      )
+  @Get(':status')
+  findAllByStatus(
+    @Param() statusDto: OrderStatusDto,
+    @Query() paginationDto: PaginationDto) {
+    return this.ordersService.send('findAllOrders', { 
+      ...paginationDto,
+      status: statusDto.status
+    }).pipe(
+      catchError(error => { throw new RpcException(error) })
+    )
+  }
+
+  @Get('id/:id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.ordersService.send('findOneOrder', id
+    ).pipe(
+      catchError(error => { throw new RpcException(error) })
+    )
+  }
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() statusDto: OrderStatusDto
+  ) {
+    return this.ordersService.send('changeOrderStatus', { id, status: statusDto.status });
   }
 }
