8d3199b1b73372ac0c03584871c40b7a3d30e2ea
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-02-21 14:39:10 -0300

    Add PaidOrderDto for handling paid order details and update index export

---
+++ orders/src/orders/dto/paid-order.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsUrl, IsUUID, } from "class-validator";
+
+export class PaidOrderDto {
+  @IsString()
+  stripePaymentId: string;
+
+  @IsUUID()
+  orderId: boolean;
+
+  @IsString()
+  @IsUrl()
+  receiptUrl: String;
+}
