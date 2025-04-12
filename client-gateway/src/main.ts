e82687441b2a94aeea997d8c0e75716f4d619d74
TomyTiseira <tomy.tiseira.22hh@gmail.com>
2025-04-12 12:07:21 -0300

    Update test comment for clarity in main.ts

--- client-gateway/src/main.ts
+++ client-gateway/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
 
-  // test
+  // test 2
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
